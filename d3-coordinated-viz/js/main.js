4d0d836d3aa389ace239249e49549b52ae3371e1
yunlei0930 <46972608+yunlei0930@users.noreply.github.com>
2019-04-11 22:40:22 -0500

    Module 9 assignment completed

--- d3-coordinated-viz/js/main.js
+++ d3-coordinated-viz/js/main.js
@@ -1,5 +1,12 @@
 /* D3 java script by Yunlei Liang, 2019 */
 
+//First line of main.js...wrap everything in a self-executing anonymous function to move to local scope
+(function(){
+
+//pseudo-global variables
+var attrArray = ["Pop", "GRP", "Unemployment", "Revenue", "ForestCoverage"];//list of attributes
+var expressed = attrArray[0]; //initial attribute
+
 //begin script when window loads
 window.onload = setMap();
 
@@ -7,7 +14,7 @@ window.onload = setMap();
 function setMap(){
 
     //map frame dimensions
-    var width = 960,
+    var width = window.innerWidth * 0.5,
         height = 460;
 
     //create new svg container for the map
@@ -32,45 +39,240 @@ function setMap(){
     promises.push(d3.csv("data/data.csv")); //load attributes from csv
     promises.push(d3.json("data/data.topojson")); //load choropleth spatial data
     Promise.all(promises).then(callback);
-	
-	function callback(data){
+
+    function callback(data){
 		csvData = data[0];
 		china = data[1];
+
+        //place graticule on the map
+        setGraticule(map, path);
 	
-	    //translate europe TopoJSON
+	    //translate china TopoJSON
    		var province = topojson.feature(china, china.objects.data).features;
         console.log(csvData);
         console.log(province);
+
+        //join csv data to GeoJSON enumeration units
+        province = joinData(province, csvData);
+
+        //create the color scale
+        var colorScale = makeColorScale(csvData);
+
+        //add enumeration units to the map
+        setEnumerationUnits(province, map, path,colorScale);
         
-        //add China regions to map
-        var Chinaprovince = map.selectAll(".Chinaprovince")
-            .data(province)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "Chinaprovince " + d.properties.ID_1;
-            })
-            .attr("d", path);
-            
-        //create graticule generator
-        var graticule = d3.geoGraticule()
-            .step([10, 10]); //place graticule lines every 10 degrees of longitude and latitude
-            
-        //create graticule background
-        var gratBackground = map.append("path")
-            .datum(graticule.outline()) //bind graticule background
-            .attr("class", "gratBackground") //assign class for styling
-            .attr("d", path) //project graticule        
-        
-        //create graticule lines
-        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
-            .data(graticule.lines()) //bind graticule lines to each element to be created
-            .enter() //create an element for each datum
-            .append("path") //append each element to the svg as a path element
-            .attr("class", "gratLines") //assign class for styling
-            .attr("d", path); //project graticule lines
+        //add coordinated visualization to the map
+    	setChart(csvData, colorScale);
+    };
+    
+}; //end of setMap()
+
+function setGraticule(map, path){
+	//create graticule generator
+	var graticule = d3.geoGraticule()
+		.step([10, 10]); //place graticule lines every 10 degrees of longitude and latitude
+		
+	//create graticule background
+	var gratBackground = map.append("path")
+		.datum(graticule.outline()) //bind graticule background
+		.attr("class", "gratBackground") //assign class for styling
+		.attr("d", path) //project graticule        
+	
+	//create graticule lines
+	var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+		.data(graticule.lines()) //bind graticule lines to each element to be created
+		.enter() //create an element for each datum
+		.append("path") //append each element to the svg as a path element
+		.attr("class", "gratLines") //assign class for styling
+		.attr("d", path); //project graticule lines
+};
+
+function joinData(province, csvData){
+	//loop through csv to assign each set of csv attribute values to geojson region
+	for (var i=0; i<csvData.length; i++){
+		var csvRegion = csvData[i]; //the current region
+		var csvKey = csvRegion.ID; //the CSV primary key
+
+		//loop through geojson regions to find correct region
+		for (var a=0; a<province.length; a++){
+
+			var geojsonProps = province[a].properties; //the current region geojson properties
+			var geojsonKey = geojsonProps.ID_1; //the geojson primary key
+
+			//where primary keys match, transfer csv data to geojson properties object
+			if (geojsonKey == csvKey){
+
+				//assign all attributes and values
+				attrArray.forEach(function(attr){
+					var val = parseFloat(csvRegion[attr]); //get csv attribute value
+					geojsonProps[attr] = val; //assign attribute and value to geojson properties
+				});
+			};
+		};
+	};
+
+    return province;
+};
+
+function setEnumerationUnits(province, map, path,colorScale){
+	//add China regions to map
+	var Chinaprovince = map.selectAll(".Chinaprovince")
+		.data(province)
+		.enter()
+		.append("path")
+		.attr("class", function(d){
+			return "Chinaprovince " + d.properties.ID_1;
+		})
+		.attr("d", path)
+        .style("fill", function(d){
+            return choropleth(d.properties, colorScale);
+        });
+
+};
+//function to create color scale generator
+function makeColorScale(data){
+    var colorClasses = [
+        "#fee5d9",
+        "#fcae91",
+        "#fb6a4a",
+        "#de2d26",
+        "#a50f15"
+    ];
+
+    //create color scale generator
+    var colorScale = d3.scaleQuantile()
+        .range(colorClasses);
+
+    //build array of all values of the expressed attribute
+    var domainArray = [];
+    for (var i=0; i<data.length; i++){
+        var val = parseFloat(data[i][expressed]);
+        domainArray.push(val);
+    };
+
+    //assign array of expressed values as scale domain
+    colorScale.domain(domainArray);
+
+    return colorScale;
+};
+
+//function to test for data value and return color
+function choropleth(props, colorScale){
+    //make sure attribute value is a number
+    var val = parseFloat(props[expressed]);
+    //if attribute value exists, assign a color; otherwise assign gray
+    if (typeof val == 'number' && !isNaN(val)){
+        return colorScale(val);
+    } else {
+        return "#CCC";
     };
 };
+
+//function to create coordinated bar chart
+function setChart(csvData, colorScale){
+    //chart frame dimensions
+    var chartWidth = window.innerWidth * 0.45,
+        chartHeight = 473,
+        leftPadding = 1,
+        rightPadding = 1,
+        topBottomPadding = 5,
+        chartInnerWidth = chartWidth - leftPadding - rightPadding,
+        chartInnerHeight = chartHeight - topBottomPadding,
+        translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+
+    //create a second svg element to hold the bar chart
+    var chart = d3.select("body")
+        .append("svg")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight)
+        .attr("class", "chart");
+
+    //create a rectangle for chart background fill
+    var chartBackground = chart.append("rect")
+        .attr("class", "chartBackground")
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
+        .attr("transform", translate);
+        
+    //create a scale to size bars proportionally to frame
+    var yScale = d3.scaleLinear()
+        .range([443,0])
+        .domain([0, 12000]);
+                
+    //set bars for each province
+    var bars = chart.selectAll(".bars")
+        .data(csvData)
+        .enter()
+        .append("rect")
+        .sort(function(a, b){
+            return a[expressed]-b[expressed]
+        })
+        .attr("class", function(d){
+            return "bars " + d.ID;
+        })
+        .attr("width", chartWidth / csvData.length - 1)
+        .attr("x", function(d, i){
+            return i * (chartWidth / csvData.length);
+        })
+        .attr("height", function(d){
+            return yScale(parseFloat(d[expressed]));
+        })
+        .attr("y", function(d){
+            return chartHeight - yScale(parseFloat(d[expressed]));
+        })
+        .style("fill", function(d){
+            return choropleth(d, colorScale);
+        });
+
+    //annotate bars with attribute value text
+    var numbers = chart.selectAll(".numbers")
+        .data(csvData)
+        .enter()
+        .append("text")
+        .sort(function(a, b){
+            return a[expressed]-b[expressed]
+        })
+        .attr("class", function(d){
+            return "numbers " + d.ID;
+        })
+        .attr("text-anchor", "middle")
+        .attr("x", function(d, i){
+            var fraction = chartWidth / csvData.length;
+            return i * fraction + (fraction - 1) / 2;
+        })
+        .attr("y", function(d){
+            return chartHeight - yScale(parseFloat(d[expressed])) + 15;
+        })
+        .text(function(d){
+            return d[expressed];
+        });
+        
+    var chartTitle = chart.append("text")
+        .attr("x", 20)
+        .attr("y", 40)
+        .attr("class", "chartTitle")
+        .text("Number of Variable " + expressed + " in each region");
+        
+    //create vertical axis generator
+    var yAxis = d3.axisLeft()
+        .scale(yScale);
+
+    //place axis
+    var axis = chart.append("g")
+        .attr("class", "axis")
+        .attr("transform", translate)
+        .call(yAxis);
+
+    //create frame for chart border
+    var chartFrame = chart.append("rect")
+        .attr("class", "chartFrame")
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
+        .attr("transform", translate);
+};
+
+})(); //last line of main.js
+
 
 
 
